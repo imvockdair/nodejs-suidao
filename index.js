0f1e624d00f8baf2112b9479db02770dcf71d96d
imvockdair <imvockdair@outlook.com>
2025-12-03 21:57:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const UUID = process.env.UUID || '8149783b-35e2-4c49-b3f9-a0592afb5bd5'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'nnn.freenpn.dpdns.org';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOWRlMjk1OTc2NDcyNjI5N2FjODc3MDA1MDY4NjY2N2IiLCJ0IjoiYzlmMDliYjItYTQyNS00N2E0LTg5MGMtNmZiZDE0ZGFiYzE5IiwicyI6Ik9EQTBaVGN5WmpjdE16a3lNeTAwTURCakxXSTBPVE10TWpjMlptVmlOR0l3TUdFNSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'frennn-cf-databricks';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
